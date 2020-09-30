cbf75feff0e12567933995fd72fd308c0127aad2
prasadakansha <49113221+prasadakansha@users.noreply.github.com>
2020-10-01 00:11:18 +0530

    Add files via upload

---
+++ theatermgt (Integrated)/src/main/java/org/cap/theatermgt/entities/Movie.java
@@ -0,0 +1,91 @@
+package org.cap.theatermgt.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue
+	@Column(name="movie_id")
+	private int movieId;
+	private String movieName;
+	private String movieDirector;
+	private int movieLength;
+	private String movieReleaseDate;
+	private String languages;
+	private String movieGenre;
+	
+	public int getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public String getMovieDirector() {
+		return movieDirector;
+	}
+
+	public void setMovieDirector(String movieDirector) {
+		this.movieDirector = movieDirector;
+	}
+
+	public int getMovieLength() {
+		return movieLength;
+	}
+
+	public void setMovieLength(int movieLength) {
+		this.movieLength = movieLength;
+	}
+
+	public String getMovieReleaseDate() {
+		return movieReleaseDate;
+	}
+
+	public void setMovieReleaseDate(String movieReleaseDate) {
+		this.movieReleaseDate = movieReleaseDate;
+	}
+
+	public String getMovieGenre() {
+		return movieGenre;
+	}
+
+	public void setMovieGenre(String movieGenre) {
+		this.movieGenre = movieGenre;
+	}
+
+	public String getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String languages) {
+		this.languages = languages;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [movieId=" + movieId + ", movieName=" + movieName + ", movieDirector=" + movieDirector
+				+ ", movieLength=" + movieLength + ", movieReleaseDate=" + movieReleaseDate + ", languages=" + languages
+				+ ", movieGenre=" + movieGenre + "]";
+	}
+	
+	
+
+
+}
